fc5de6927fe4e630753b2b3b4d4e00548dff215f
Niklas Schlimm <niklas.schlimm@provinzial.com>
2011-07-21 16:46:55 +0200

    Runder Teststand mehrere Chains

--- sources/spring-decorator/src/test/java/com/schlimm/decorator/resolver/longtwochains/IntegrationTests_AnotherDelegate.java
+++ sources/spring-decorator/src/test/java/com/schlimm/decorator/resolver/longtwochains/IntegrationTests_AnotherDelegate.java
@@ -11,8 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.schlimm.decorator.resolver.longsinglechain.MyDelegate;
-
 @ContextConfiguration("/test-context-decorator-resolver-long-two-chains-integration.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
 public class IntegrationTests_AnotherDelegate {
@@ -28,22 +26,24 @@ public class IntegrationTests_AnotherDelegate {
 	@Test
 	public void testInjectedObjectIsDecorator() {
 		Assert.assertTrue(AopUtils.getTargetClass(decoratedInterface).getAnnotation(Decorator.class) != null);
-		Assert.assertTrue(decoratedInterface.getDelegateObject().getClass().getSimpleName().startsWith("Another"));
+		Assert.assertTrue(decoratedInterface.getClass().getSimpleName().startsWith("AnotherDecorator"));
 	}
 
 	@Test
 	public void testSecondDecorator() {
-		Assert.assertTrue(decoratedInterface.getDelegateObject().getClass().getAnnotation(Decorator.class) != null);
+		Assert.assertTrue(AopUtils.getTargetClass(decoratedInterface.getDelegateObject()).getAnnotation(Decorator.class)!= null);
+		Assert.assertTrue(decoratedInterface.getDelegateObject().getClass().getSimpleName().startsWith("AnotherDecorator"));
 	}
 
 	@Test
 	public void testThirdDecorator() {
-		Assert.assertTrue(decoratedInterface.getDelegateObject().getDelegateObject().getClass().getAnnotation(Decorator.class) != null);
+		Assert.assertTrue(AopUtils.getTargetClass(decoratedInterface.getDelegateObject().getDelegateObject()).getAnnotation(Decorator.class)!= null);
+		Assert.assertTrue(decoratedInterface.getDelegateObject().getDelegateObject().getClass().getSimpleName().startsWith("AnotherDecorator"));
 	}
 
 	@Test
 	public void testDelegateTargetObject() {
-		Assert.assertTrue(MyDelegate.class.isAssignableFrom(decoratedInterface.getDelegateObject().getDelegateObject().getDelegateObject().getClass()));
+		Assert.assertTrue(AnotherDelegate.class.isAssignableFrom(decoratedInterface.getDelegateObject().getDelegateObject().getDelegateObject().getClass()));
 	}
 
 	@Test
